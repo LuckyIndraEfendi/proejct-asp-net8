1a521670a217ee396e05d4cd51d863b0d6671bf6
Lucky Indra Efendi <kimilbonchu@gmail.com>
2024-07-02 11:48:30 +0700

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using Dashboard_Admin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using BCrypt.Net;
+namespace Dashboard_Admin.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly string connectionString = "";
+        private readonly string salt = BCrypt.Net.BCrypt.GenerateSalt(10);
+        public AuthController(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("DefaultConnection");
+
+        }
+        [HttpPost]
+        [Route("auth/signin")]
+        public async Task<IActionResult> SignIn([FromBody] AuthSignUpModel signup)
+        {
+            var conn = new SqlConnection(connectionString);
+            if (conn.State == ConnectionState.Closed) await conn.OpenAsync();
+
+            var trx = conn.BeginTransaction();
+            try
+            {
+                var p = new DynamicParameters();
+                p.Add("@Username", signup.username);
+                p.Add("@Email", signup.email);
+                p.Add("@Password", BCrypt.Net.BCrypt.HashPassword(signup.password,salt));
+
+                var result = await conn.ExecuteAsync("usp_InsertCustomer", p, commandType: CommandType.StoredProcedure,transaction: trx);
+             
+                await trx.CommitAsync();
+
+                return Json(new { status_code = 200, message = "Registration Successful",data= result });
+
+            }
+            catch (Exception err)
+            {
+                return StatusCode(500, new { status_code = 500, message = err.Message });
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ Models/AuthModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Dashboard_Admin.Models
+{
+    public class AuthSignUpModel
+    {
+        public int customerId { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+
+    }
+}
